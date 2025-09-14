ef93d1da15ba39cd63b00969e6a91d79eb893a4e
Jean Victor <jeanvictor.fa@gmail.com>
2025-09-14 14:41:47 -0300

    feat: recovering password usecase

---
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/AuthResource.java
@@ -0,0 +1,36 @@
+package com.devsuperior.dscatalog.resources;
+
+import com.devsuperior.dscatalog.dtos.*;
+import com.devsuperior.dscatalog.services.AuthService;
+import com.devsuperior.dscatalog.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping(value = "/recover-token")
+    public ResponseEntity<Void> createRecoverTOken(@Valid @RequestBody EmailDTO body){
+        authService.createRecoverToken(body);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/new-password")
+    public ResponseEntity<Void> saveNrePassword(@Valid @RequestBody NewPasswordDTO body){
+        authService.saveNewPassword(body);
+        return ResponseEntity.noContent().build();
+    }
+
+}
